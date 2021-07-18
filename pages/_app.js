ee3a7ccda59f3722c22ab98b69f29d6593113353
Bayu Slari <bayu93mx@gmail.com>
2021-07-18 15:05:52 +0700

    Handle router change for GA. Closes #18 (#19)

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
 import { AnimatePresence } from 'framer-motion';
 import withDarkMode, { useDarkMode } from 'next-dark-mode';
 import { SmoothScrollProvider } from '../components/scroll';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import * as gtag from '../lib/gtag';
 import '../scss/styles.scss';
 
-const MyApp = ({ Component, pageProps, router }) => {
+const MyApp = ({ Component, pageProps }) => {
   const { darkModeActive } = useDarkMode();
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <AnimatePresence exitBeforeEnter>
       <div className={darkModeActive ? 'darkMode' : 'lightMode'}>
