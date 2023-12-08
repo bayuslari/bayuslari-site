e919afbab066dd75c04d64601a3c38cf54b000d6
bayuslari <bayu93mx@gmail.com>
2023-12-09 01:34:19 +0700

    Under maintenance

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   images: {
     domains: ['i.ibb.co']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/((?!maintenance).*)',
+        destination: '/maintenance.html',
+        permanent: false //!!!IMPORTANT!!!
+      }
+    ];
   }
 };
---
+++ pages/maintenance.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Maintenance = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        height: '100vh',
+        alignItems: 'center',
+        justifyContent: 'center'
+      }}
+    >
+      <h1>Under Maintenance</h1>
+    </div>
+  );
+};
+
+export default Maintenance;
